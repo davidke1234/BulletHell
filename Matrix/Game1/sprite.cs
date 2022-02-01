2d4c46a6f2a1e19889f67a7aee4a7c50f18c2f3d
whoke <whoke@wsu.edu>
2022-02-01 12:32:54 -0800

    Working on player movement.

---
+++ Matrix/Game1/sprite.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game1
+{
+    public class Sprite
+    {
+        private Texture2D _texture;
+        public Vector2 _position;
+
+        public float _speed = 2f;
+
+        public Sprite(Texture2D texture)
+        {
+            _texture = texture;
+        }
+
+        public void Update()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                //move sprite up
+                _position.Y -= _speed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                //move sprite down
+                _position.Y += _speed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                //move sprite left
+                _position.X -= _speed;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                //move sprite right
+                _position.X += _speed;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _position, Color.White);
+        }
+    }
+}
