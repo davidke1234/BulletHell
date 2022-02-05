9161276503a6c701dc9d2980ce1c8215d9a89d2f
Afoke Abogidi <afoke.abogidi@wsu.edu>
2022-02-05 01:44:24 -0800

    Added a bomb entity. Not yet in use

---
+++ Matrix/Game1/Bombs.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game1
+{
+    class Bombs : Sprite
+    {
+        private static Bombs _instance;
+
+        /// <summary>
+        /// Provides an instance of the Bombs class
+        /// </summary>
+        public static Bombs Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Bombs();
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="Bombs" class./>
+        /// </summary>
+        private Bombs()
+        {
+            image = Arts.Bomb;
+            Position.X = 0;
+            Position.Y = 0;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            var bossInstance = MidBoss.Instance;
+            if (!MidBoss.Instance.IsOutdated)
+            {
+                Position.X = bossInstance.Position.X;
+                Position.Y = bossInstance.Position.Y;
+            }
+
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>        
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(image, Position, Color.White);
+        }
+    }
+}
